4256b37d485ac5e4ad09640107ba7e7940c367fb
linkyo4 <160897+linkyo4@users.noreply.github.com>
2020-07-24 17:37:08 +0700

    qlchitieu
    quản lý chi tiêu

---
+++ QuanLyChiTieu/app/src/main/java/com/example/ps10389_lequangminh_assigment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ps10389_lequangminh_assigment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
